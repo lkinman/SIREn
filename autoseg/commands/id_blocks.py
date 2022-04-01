5fba9c31ceb0bd3615f8ec05c5dac68b553c8c68
Laurel Kinman <47401759+lkinman@users.noreply.github.com>
2022-04-01 12:58:14 -0400

    Update id_blocks.py

--- autoseg/commands/id_blocks.py
+++ autoseg/commands/id_blocks.py
@@ -17,11 +17,13 @@ from autoseg import funcs
 def add_args(parser):
     parser.add_argument('--config', type = str,  required = True, help = 'Path to sketch_communities.py config file')
     parser.add_argument('--blockdir', type = str, required = True, help = 'Path to directory where segmented blocks are stored')
+    parser.add_argument('--p', type = float, default = 99, help = 'Percentile occupancy of blocks to set as high threshold')
     return parser
 
 def main(args):
     configs_dict = utils.load_pkl(args.config)
     outdir = funcs.check_dir(configs_dict['outdir'])
+    ided_outdir = funcs.check_dir(configs_dict['outdir'] + '03_blocks', make = True)
     voldir = funcs.check_dir(configs_dict['voldir'])
     blockdir = funcs.check_dir(args.blockdir)
     vol_list = np.sort(glob.glob(voldir + '*.mrc'))
@@ -32,7 +34,7 @@ def main(args):
     blocks_dict = funcs.read_blocks(blockdir, union_voxels)
     for i in blocks_dict:
         occs = np.sum(binned[:, blocks_dict[i]], axis = 1)/len(blocks_dict[i])
-        high_thr = np.percentile(np.sum(binned[:, blocks_dict[i]], axis = 1)/len(blocks_dict[i]), 99)
+        high_thr = np.percentile(np.sum(binned[:, blocks_dict[i]], axis = 1)/len(blocks_dict[i]), args.p)
         high_occs = np.where(occs > high_thr)[0]
         low_occs = np.where(occs == 0)[0]
         high_occs_union = np.where(np.sum(binned[high_occs, :], axis = 0) == len(high_occs), 1, 0)
@@ -48,7 +50,8 @@ def main(args):
             data[corrgroup] = 1
             mrc.write(outfile, data.reshape(64, 64, 64), header = head)
             
-    
+        configs_dict['p'] = args.p
+        utils.save_pkl(configs_dict, ided_outdir + 'configs_dict.pkl')
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     main(add_args(parser).parse_args())
